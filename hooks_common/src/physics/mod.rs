4a85bb2f3d314269a19685ffcc1b29b65ffcbddc
leod <subtle.frustration@proton.me>
2017-12-16 15:37:36 +0100

    Implement RemoveCollisionObject

--- hooks_common/src/physics/mod.rs
+++ hooks_common/src/physics/mod.rs
@@ -4,7 +4,7 @@ use nalgebra::{Point2, Isometry2};
 use ncollide::world::{CollisionGroups, CollisionWorld2, GeometricQueryType};
 use ncollide::shape::ShapeHandle2;
 
-// Signal components
+// Tag components
 #[derive(Component)]
 #[component(HashMapStorage)]
 pub struct CreateCollisionObject {
@@ -39,6 +39,7 @@ pub struct Orientation {
     pub angle: f32,
 }
 
+// System for creating collision objects for entities tagged with CreateCollisionObject
 pub struct CreateCollisionObjectSys {
     next_uid: usize
 }
@@ -63,22 +64,49 @@ impl<'a> System<'a> for CreateCollisionObjectSys {
                        WriteStorage<'a, CollisionObjectUid>);
 
     fn run(&mut self, (mut collision_world, entities, position, orientation, shape, mut create_object, mut object_uid): Self::SystemData) {
-        for (entity, position, orientation, shape, create_object) in (&*entities, &position, &orientation, &shape, &mut create_object).join() {
-            let uid = self.next_uid;
-            self.next_uid += 1;
+        let created_entities =
+            (&*entities, &position, &orientation, &shape, &create_object).join().map(|(entity, position, orientation, shape, create_object)| {
+                let uid = self.next_uid;
+                self.next_uid += 1;
 
-            object_uid.insert(entity, CollisionObjectUid(uid));
+                let isometry = Isometry2::new(position.pos.coords, 
+                                              orientation.angle);
+                collision_world.deferred_add(uid, 
+                                             isometry,
+                                             shape.shape.clone(),
+                                             create_object.collision_groups,
+                                             create_object.query_type,
+                                             entity);
 
-            let isometry = Isometry2::new(position.pos.coords, 
-                                          orientation.angle);
-            collision_world.deferred_add(uid, 
-                                         isometry,
-                                         shape.shape.clone(),
-                                         create_object.collision_groups,
-                                         create_object.query_type,
-                                         entity);
+                object_uid.insert(entity, CollisionObjectUid(uid));
+
+                entity
+            }).collect::<Vec<_>>();
+
+        for entity in created_entities {
+            create_object.remove(entity);
         }
     }
 }
 
+// System for removing collision objects for entities tagged with RemoveCollisionObject
 struct RemoveCollisionObjectSys;
+
+impl<'a> System<'a> for RemoveCollisionObjectSys {
+    type SystemData = (FetchMut<'a, CollisionWorld>,
+                       Entities<'a>,
+                       WriteStorage<'a, RemoveCollisionObject>,
+                       WriteStorage<'a, CollisionObjectUid>);
+
+    fn run(&mut self, (mut collision_world, entities, mut remove_object, mut object_uid): Self::SystemData) {
+        let removed_entities = (&*entities, &mut remove_object, &mut object_uid).join().map(|(entity, _, object_uid)| {
+            collision_world.deferred_remove(object_uid.0);
+            entity
+        }).collect::<Vec<_>>();
+
+        for entity in removed_entities {
+            remove_object.remove(entity);
+            object_uid.remove(entity);
+        }
+    }
+}
