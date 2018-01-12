ff869ec3064790fd461a797f3989e82d0632c38b
Leonard Dahlmann <leo.dahlmann@gmail.com>
2018-01-13 00:40:45 +0100

    Actually connect to the server. Add Makefile for running both server and client in tmux.

---
+++ Makefile
@@ -0,0 +1,19 @@
+TARGET := "target/"
+
+all: build
+
+build: common server game
+
+run: build
+	tmux \
+		new-session 'bash -c "RUST_BACKTRACE=1 target/debug/hooks_server; cat"' \; \
+		split-window -h 'bash -c "RUST_BACKTRACE=1 target/debug/hooks_game; cat"'
+
+common:
+	CARGO_TARGET_DIR=${TARGET} cargo build -j4 --manifest-path=hooks_common/Cargo.toml
+
+server:
+	CARGO_TARGET_DIR=${TARGET} cargo build -j4 --manifest-path=hooks_server/Cargo.toml
+
+game:
+	CARGO_TARGET_DIR=${TARGET} cargo build -j4 --manifest-path=hooks_game/Cargo.toml
--- hooks_game/src/main.rs
+++ hooks_game/src/main.rs
@@ -1,3 +1,30 @@
+extern crate bit_manager;
+extern crate hooks_common as common;
+#[macro_use]
+extern crate log;
+extern crate simple_logger;
+
+mod client;
+
+use client::Client;
+
+struct Config {
+    host: String,
+    port: u16,
+    name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    simple_logger::init().unwrap();
+
+    let config = Config {
+        host: "localhost".to_string(),
+        port: 32444,
+        name: "testy".to_string(),
+    };
+    let timeout_ms = 5000;
+
+    let mut client = Client::connect(&config.host, config.port, &config.name, timeout_ms).unwrap();
+
+    info!("Connected to {}:{}", config.host, config.port);
 }
