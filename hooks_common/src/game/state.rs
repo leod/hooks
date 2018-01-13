6c57bb9efecbccc95a9cc36e215ed471bfe09884
leod <subtle.frustration@proton.me>
2018-01-13 21:11:40 +0100

    Add State struct for holding game state and running ticks

---
+++ hooks_common/src/game/state.rs
@@ -0,0 +1,36 @@
+use specs::{World, Dispatcher};
+
+use event::{self, Event};
+use registry::{Registry, EventHandler};
+
+pub struct State {
+    pub world: World,
+    pub event_reg: event::Registry,
+    pub tick_dispatcher: Dispatcher<'static, 'static>,
+    pub event_handlers_post_tick: Vec<EventHandler>,
+}
+
+impl State {
+    pub fn from_registry(reg: Registry) -> State {
+        State {
+            world: reg.world,
+            event_reg: reg.event_reg,
+            tick_dispatcher: reg.tick_systems.build(),
+            event_handlers_post_tick: reg.event_handlers_post_tick
+        }
+    }
+
+    pub fn run_tick(&mut self) -> Vec<Box<Event>> {
+        self.tick_dispatcher.dispatch_seq(&self.world.res);
+
+        let events = self.world.write_resource::<event::Sink>().clear();
+
+        for event in &events {
+            for handler in &self.event_handlers_post_tick {
+                handler(&mut self.world, event);
+            }
+        }
+
+        events
+    }
+}
