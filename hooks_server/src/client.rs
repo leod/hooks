065338b9dc2e828adcce89a1b976c264e81c62c7
leod <subtle.frustration@proton.me>
2018-01-13 22:14:51 +0100

    Better handling of player connections in Host

--- hooks_server/src/client.rs
+++ hooks_server/src/client.rs
@@ -3,24 +3,34 @@ use common::net::transport;
 
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub enum State {
+    /// We have acknowledged the connection and sent game info.
     Connected,
 
-    /// We forcefully disconnected the client.
-    Disconnected,
+    /// The client has received the game info and is ready to receive ticks.
+    Ready,
 }
 
 pub struct Client {
     pub peer: transport::Peer,
+    pub name: String,
     pub state: State,
     pub last_ack_tick: Option<TickNum>,
 }
 
 impl Client {
-    pub fn new(peer: transport::Peer) -> Client {
+    pub fn new(peer: transport::Peer, name: String) -> Client {
         Client {
             peer,
+            name,
             state: State::Connected,
             last_ack_tick: None,
+        }
+    }
+
+    pub fn ingame(&self) -> bool {
+        match self.state {
+            State::Connected => false,
+            State::Ready => true,
         }
     }
 }
