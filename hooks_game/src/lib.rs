f72a47f5dc21f6e5f5fd9ad7376fb7d5069d4841
leod <subtle.frustration@proton.me>
2018-01-20 12:35:30 +0100

    Use View in MainState, split off registry part of View

--- hooks_game/src/lib.rs
+++ hooks_game/src/lib.rs
@@ -3,6 +3,7 @@ extern crate ggez;
 extern crate hooks_common as common;
 #[macro_use]
 extern crate log;
+extern crate nalgebra;
 extern crate rand;
 extern crate shred;
 extern crate specs;
--- hooks_game/src/view/mod.rs
+++ hooks_game/src/view/mod.rs
@@ -1,12 +1,16 @@
+mod camera;
+
 use specs::World;
 
 use ggez;
 
-use common::{self, Event, GameInfo, Registry};
+use common::{self, Event, GameInfo, PlayerId};
 
-pub fn register(game_info: &GameInfo, reg: &mut Registry) {}
+use self::camera::Camera;
 
-pub fn register_load(game_info: &GameInfo, view: &mut View) {}
+pub fn register(game_info: &GameInfo, reg: &mut common::Registry) {}
+
+pub fn register_load(game_info: &GameInfo, view: &mut Registry) {}
 
 #[derive(Default)]
 pub struct Assets {}
@@ -14,36 +18,53 @@ pub struct Assets {}
 pub type EventHandler = fn(&Assets, &mut World, &Vec<Box<Event>>) -> ggez::error::GameResult<()>;
 pub type DrawFn = fn(&Assets, &mut ggez::Context, &mut World) -> ggez::error::GameResult<()>;
 
-pub struct View {
-    game_info: GameInfo,
-    assets: Assets,
-    event_handlers_post_tick: Vec<EventHandler>,
+#[derive(Default)]
+pub struct Registry {
+    event_handlers: Vec<EventHandler>,
     draw_fns: Vec<DrawFn>,
 }
 
-impl View {
-    /// Load all assets for a game info and create a `View`.
-    pub fn load(game_info: &GameInfo, assets: Assets) -> View {
-        let mut view = View {
-            game_info: game_info.clone(),
-            assets: assets,
-            event_handlers_post_tick: Vec::new(),
-            draw_fns: Vec::new(),
-        };
-
-        register_load(game_info, &mut view);
-
-        view
-    }
-
+impl Registry {
     /// Register a new game event handler. Only called at the start of a game.
-    pub fn event_handler_post_tick(&mut self, f: EventHandler) {
-        self.event_handlers_post_tick.push(f);
+    pub fn event_handler(&mut self, f: EventHandler) {
+        self.event_handlers.push(f);
     }
 
     /// Register a new drawing function. Only called at the start of a game.
     pub fn draw_fn(&mut self, f: DrawFn) {
         self.draw_fns.push(f);
+    }
+}
+
+pub struct View {
+    my_player_id: PlayerId,
+    game_info: GameInfo,
+
+    assets: Assets,
+    reg: Registry,
+
+    camera: Camera,
+}
+
+impl View {
+    /// Load all assets for a game info and create a `View`.
+    pub fn load(
+        my_player_id: PlayerId,
+        game_info: &GameInfo,
+        assets: Assets,
+    ) -> ggez::error::GameResult<View> {
+        let mut reg = Registry::default();
+        register_load(game_info, &mut reg);
+
+        Ok(View {
+            my_player_id,
+            game_info: game_info.clone(),
+
+            assets,
+            reg,
+
+            camera: Camera::new(),
+        })
     }
 
     /// View game events.
@@ -52,7 +73,7 @@ impl View {
         world: &mut World,
         events: &Vec<Box<Event>>,
     ) -> ggez::error::GameResult<()> {
-        for handler in &self.event_handlers_post_tick {
+        for handler in &self.reg.event_handlers {
             handler(&self.assets, world, events)?;
         }
 
@@ -65,7 +86,11 @@ impl View {
         ctx: &mut ggez::Context,
         world: &mut World,
     ) -> ggez::error::GameResult<()> {
-        for draw_fn in &self.draw_fns {
+        let delta = ggez::timer::get_delta(ctx);
+
+        self.camera.update(delta);
+
+        for draw_fn in &self.reg.draw_fns {
             draw_fn(&self.assets, ctx, world)?;
         }
 
